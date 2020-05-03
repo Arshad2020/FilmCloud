ca25f8c9991b82b47349383f2bd33a5943aeb540
Arshad <arshad.researcher@gmail.com>
2020-05-03 12:06:20 +0200

    Updates

---
+++ precache-manifest.b1701afa818f2ce4e47f5e6956c367f7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f764f1e548c1ce7faf96cb7c8df09da",
+    "url": "/FilmCloud/index.html"
+  },
+  {
+    "revision": "88a877f28e1ca5f7d98b",
+    "url": "/FilmCloud/static/css/main.281a52bc.chunk.css"
+  },
+  {
+    "revision": "4071193ea103511d8e5a",
+    "url": "/FilmCloud/static/js/2.791078da.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/FilmCloud/static/js/2.791078da.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "88a877f28e1ca5f7d98b",
+    "url": "/FilmCloud/static/js/main.dd9e355f.chunk.js"
+  },
+  {
+    "revision": "4ba49e25a4a61ed75b67",
+    "url": "/FilmCloud/static/js/runtime-main.d17c1844.js"
+  },
+  {
+    "revision": "a7bbc4d508d96741e6edd754d93479ba",
+    "url": "/FilmCloud/static/media/logo.a7bbc4d5.png"
+  }
+]);
